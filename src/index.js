dd4b91ee8fd835d4d974831e0e00b713eedc7132
Alexey Ryzhkov <ryzhkov@scand.com>
2018-09-20 14:42:24 +0300

    feat: implement feature getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0;
+  for(let i = 1; i < number + 1; i++){
+    if(i%5===0){
+      count += getCount(i);
+    }
+  }
+  return count;
+};
+
+function getCount(number){
+  count = 0;
+  if(number%5===0){
+    count += 1 + getCount(number/5);
+  }
+  return count;
 }
