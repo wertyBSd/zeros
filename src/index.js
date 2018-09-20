0c571eeed40eea646975b4fc56a649c46c0b230d
Alexey Ryzhkov <ryzhkov@scand.com>
2018-09-20 14:58:01 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function getZerosCount(number) {
   let count = 0;
-  for(let i = 1; i < number + 1; i++){
+  for(let i = 5; i < number + 1; i += 5){
     if(i%5===0){
       count += getCount(i);
     }
